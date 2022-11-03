a06d4f3bb4051c7aa9993687f92fb2bc2b6804aa
HawraaYounes <hawraayounes0@gmail.com>
2022-11-03 14:39:33 +0200

    Backend: Adding HistoryController group routes in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,18 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemperatureController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\HistoryController;
+
+//TemperatureController
 Route::controller(TemperatureController::class)->group(function () {
     Route::post('/temperature', "sendTemperature");
     Route::get('/temperature',"getTemperatures");
     Route::get('/last-temperature',"getLastTemperature");
 });
+//HistoryController
+Route::controller(HistoryController::class)->group(function () {
+   
+});
+
