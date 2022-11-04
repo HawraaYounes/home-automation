1c5820114fd28162574d980734712bdde06f1d91
HawraaYounes <hawraayounes0@gmail.com>
2022-11-04 07:46:45 +0200

    Backend: Adding getUsers function in UserController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function getUsers(){
+        $users=User::all();
+        return response()->json($users, 201);
+    }
 }
