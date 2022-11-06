9c3271569b7c7d9008d20dca64e29a55b67c6fdc
HawraaYounes <hawraayounes0@gmail.com>
2022-11-06 21:55:56 +0200

    Backend: Adding getAlbums route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\TemperatureController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AlbumController;
+
 //TemperatureController
 Route::controller(TemperatureController::class)->group(function () {
     Route::post('/temperature', "sendTemperature");
@@ -25,4 +27,8 @@ Route::controller(PasswordController::class)->group(function () {
 //UserController
 Route::controller(UserController::class)->group(function () {
     Route::get('/users',"getUsers");
+});
+//AlbumController
+Route::controller(AlbumController::class)->group(function () {
+    Route::get('/albums',"getAlbums");
 });
