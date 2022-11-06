6147d4a041a664e6ed06b57cf5a7df50e9819bb5
HawraaYounes <hawraayounes0@gmail.com>
2022-11-07 00:10:34 +0200

    Backend: Adding getMemories function in MemoryController

--- backend/app/Http/Controllers/MemoryController.php
+++ backend/app/Http/Controllers/MemoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memory;
 use Illuminate\Http\Request;
 
 class MemoryController extends Controller
 {
-    //
+    public function getMemories($id){
+        $memories = Memory::where('album_id',$id);
+        return response()->json($memories, 201);
+    }
 }
