646c624151eb8f5362333a52c05ec9aa94ac16e6
HawraaYounes <hawraayounes0@gmail.com>
2022-11-06 22:00:26 +0200

    Backend: Adding getAlbums function in AlbumController

--- backend/app/Http/Controllers/AlbumController.php
+++ backend/app/Http/Controllers/AlbumController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Album;
 
 class AlbumController extends Controller
 {
-    
+    public function getAlbums(){
+        $albums = Album::all();
+        return response()->json($albums, 201);
+    }
 }
