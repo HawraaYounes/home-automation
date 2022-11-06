eb9c0242785d26bdba60ee7fbed118010578b54a
HawraaYounes <hawraayounes0@gmail.com>
2022-11-07 00:20:08 +0200

    Backend: Updating getMemories function in MemoryController

--- backend/app/Http/Controllers/MemoryController.php
+++ backend/app/Http/Controllers/MemoryController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Memory;
 use Illuminate\Http\Request;
+use Validator;
 
 class MemoryController extends Controller
 {
     public function getMemories($id){
-        $memories = Memory::where('album_id',$id);
+        $memories = Memory::where('album_id',$id)->get();
         return response()->json($memories, 201);
     }
 
+    
 }
