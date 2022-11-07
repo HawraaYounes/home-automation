47f25cbf50f266d669d5df2b11d40d013be52fa8
HawraaYounes <hawraayounes0@gmail.com>
2022-11-07 12:26:05 +0200

    Backend: Adding image with addMemory function in MemoryController

--- backend/app/Http/Controllers/MemoryController.php
+++ backend/app/Http/Controllers/MemoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\Memory;
 use Illuminate\Http\Request;
 use Validator;
@@ -22,7 +23,28 @@ class MemoryController extends Controller
         if($validator->fails()){
             return response()->json($validator->errors()->toJSON(), 200);
         }
-        Memory::create($validator->validated());
+        $memory=Memory::create($validator->validated());
+
+        if($request->hasFile('images')) {
+        $allowedfileExtension=['jpeg','jpg','png'];
+        $files = $request->file('images'); 
+        foreach ($files as $file) {      
+            $extension = $file->getClientOriginalExtension();
+            $check = in_array($extension,$allowedfileExtension);
+            if($check) {
+                foreach($request->images as $mediaFiles) {
+                    $path = $mediaFiles->store('public/memories');
+                    $image = new Image();
+                    $image->memory_id= $memory->id;
+                    $image->path = $path;
+                    $image->save();
+                    return response()->json(['Memory added with image'], 201);
+                }
+            } else {
+                return response()->json(['invalid_file_format'], 422);
+            }
+        }
+    }
         return response()->json(["message" => 'Memory added'], 201);
     }
 }
