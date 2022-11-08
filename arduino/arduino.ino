ea56c9fb124291657cd18bf040a267c0b36eca06
HawraaYounes <hawraayounes0@gmail.com>
2022-11-09 01:30:33 +0200

    Arduino: Adding ldr photoresistor and led Connection

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,31 @@
 int flameBuzzer=4;
 int flameSensor=8;
+int ldr=A0;
+int led=7;
 void setup() {
-pinMode ( flameBuzzer , OUTPUT ); // declaring Buzzer pin as output pin
-pinMode ( flameSensor , INPUT );  // declaring sensor pin as input pin for Arduino
+  Serial.begin(9600);
+  pinMode ( flameBuzzer , OUTPUT ); // declaring Buzzer pin as output pin
+  pinMode ( flameSensor , INPUT );  // declaring sensor pin as input pin for Arduino
+  pinMode ( ldr , INPUT );
+  pinMode ( led , OUTPUT );
 }
 
 void loop() {
- int Read = digitalRead ( flameSensor ) ; // reading from the sensor
-  if (Read == LOW ){
-  digitalWrite ( flameBuzzer , HIGH ) ;// if state is high, then turn high the Buzzer
+  int light =analogRead(A0);
+  int Read = digitalRead ( flameSensor ) ; // reading from the sensor
+  int ldrRead = digitalRead ( ldr ) ;
+   if (light <200 ){
+    digitalWrite ( led , HIGH ) ;// if state is high, then turn high the Buzzer
   }
   else{
-  digitalWrite ( flameBuzzer , LOW ) ; // otherwise turn it low
+    digitalWrite ( led , LOW ) ; // otherwise turn it low
   }
+  if (Read == LOW ){
+  
+    digitalWrite ( flameBuzzer , HIGH ) ;// if state is high, then turn high the Buzzer
+  }
+  else{
+    digitalWrite ( flameBuzzer , LOW ) ; // otherwise turn it low
+  }
+  
 }
