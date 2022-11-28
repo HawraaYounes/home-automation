24bcb59774efaa34b156d63e0c0871b490edcbfe
HawraaYounes <hawraayounes0@gmail.com>
2022-11-28 22:10:28 +0200

    Backend: Adding addHistory api in HistoryController

--- backend/app/Http/Controllers/HistoryController.php
+++ backend/app/Http/Controllers/HistoryController.php
@@ -14,5 +14,17 @@ class HistoryController extends Controller
         return response()->json($histories, 200);
     }
 
- 
+    public function addHistory(Request $request){
+        $validator = Validator::make($request->all(), [
+            'activity' => 'required',
+            'user_id' => 'required|integer'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJSON(), 200);
+        }
+        History::create($validator->validated());
+        return response()->json(["message" => 'History added'], 201);
+    }
+    
 }
