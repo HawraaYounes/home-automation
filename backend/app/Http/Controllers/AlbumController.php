c2399a294520af2ce07ce2f89c8745a12dc3528a
HawraaYounes <hawraayounes0@gmail.com>
2022-11-28 21:57:13 +0200

    Backend: Adding addAlbum in AlbumController

--- backend/app/Http/Controllers/AlbumController.php
+++ backend/app/Http/Controllers/AlbumController.php
@@ -12,5 +12,15 @@ class AlbumController extends Controller
         $albums = Album::all();
         return response()->json($albums, 201);
     }
-   
+    public function addAlbum(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJSON(), 200);
+        }
+        Album::create($validator->validated());
+        return response()->json(["message" => 'Album added'], 201);
+    }
 }
