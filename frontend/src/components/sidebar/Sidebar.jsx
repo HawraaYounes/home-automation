3be0f494c450a46127c420425a3d7620640e383d
HawraaYounes <hawraayounes0@gmail.com>
2022-11-28 23:23:47 +0200

    Frontend: Adding Sidebar component

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Sidebar.css';
+import logo from '../sidebar/assets/inhome-logo.PNG' ;
+import { IconContext } from 'react-icons';
+const Sidebar=()=>{
+    const [sidebar, setSidebar] = useState(true);
+    const showSidebr = () => setSidebar(!sidebar);
+    return(
+        <>
+          <IconContext.Provider value={{ color: '#fff' }}>
+              <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                  <li className='navbar-toggle'>
+                    <img src={logo} className='logo' alt='logo'/>
+                  </li>
+                  {SidebarData.map((item, index) => {
+                    return (
+                      <li key={index} className={item.cName} >
+                        <Link to={item.path}>
+                          <span className='icon'>{item.icon}</span>
+                          <span>{item.title}</span>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
+          </IconContext.Provider>
+       </>
+    )
+}
+export default Sidebar;
